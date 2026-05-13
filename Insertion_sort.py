55534cdde94f0cb2a6f256336842c1b3563e8b92
Emmax <olatunjie355@gmail.com>
2026-05-13 10:41:15 +0100

    Updated Insertion sorting

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,23 +1,16 @@
 def insertion_sorting(List):
     length = len(List)
     sorted_list = []
-    sorted_list.append(List)
+    
    
     for i in range(length):
-        # print("i: ", i)
-        for j in range(i + 1, length):
-            # print("j: ", j)
-            for k in range(j + 1, length):
-                # print("k: ", k)
-                if List[j][0] < List[i][0]:
-                    sorted_list.append([List[j], List[i], List[k]])
-                else:
-                    sorted_list.append(List)
-    if sorted_list[1][2][0] < sorted_list[1][1][0]:
-        sorted_list.append([sorted_list[1][0], sorted_list[1][2], sorted_list[1][1]])
-    # else:
-    #     sorted_list.append(sorted_list[1])
-    # print(sorted_list)
+        j = i - 1
+        print("FIRST j: ", j)
+        while j >= 0 and List[j][0] > List[j + 1][0]:
+            List[j], List[j + 1] = List[j + 1], List[j]
+        j -= 1
+        print("SECOND j: ", j)
+        sorted_list.append(List[:])
     return sorted_list
 
 example = [(3, "apple"), (1, "banana"), (2, "cherry")]
